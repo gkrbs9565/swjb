53f862e5387c56daf4700b539493c641b530dc98
gkrbs9565 <151802751+gkrbs9565@users.noreply.github.com>
2024-06-06 23:37:43 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,32 @@
+const $text = document.querySelector(".typing .text");
+
+// 글자 모음
+const letters = [
+    "SUWON INFORMATION HIGH SCHOOL",
+];
+
+// 글자 입력 속도
+const speed = 170;
+let i = 0;
+
+// 타이핑 효과
+const typing = async () => {
+    const letter = letters[i].split("");
+
+    while (letter.length) {
+        await wait(speed);
+        $text.innerHTML += letter.shift();
+    }
+
+    // 잠시 대기
+    await wait(800);
+
+}
+
+// 딜레이 기능 ( 마이크로초 )
+function wait(ms) {
+    return new Promise(res => setTimeout(res, ms))
+}
+
+// 초기 실행
+setTimeout(typing, 1500);
