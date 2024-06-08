786fb23f12722a2698fe30e95009e3d987bdc786
gkrbs9565 <151802751+gkrbs9565@users.noreply.github.com>
2024-06-09 05:21:54 +0900

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,44 +1,43 @@
-// 타이핑 효과 js
-const $text = document.querySelector(".typing .text");
+document.addEventListener("DOMContentLoaded", function() {
+    const $text = document.querySelector(".typing .text");
 
-// 글자 모음
-const letters = [
-    "SUWON INFORMATION HIGH SCHOOL",
-];
+    // 글자 모음
+    const letters = [
+        "SUWON INFORMATION HIGH SCHOOL",
+    ];
 
-// 글자 입력 속도
-const speed = 170;
-let i = 0;
+    // 글자 입력 속도
+    const speed = 170;
+    let i = 0;
 
-// 타이핑 효과
-const typing = async () => {
-    const letter = letters[i].split("");
+    // 타이핑 효과
+    const typing = async () => {
+        const letter = letters[i].split("");
 
-    while (letter.length) {
-        await wait(speed);
-        $text.innerHTML += letter.shift();
+        while (letter.length) {
+            await wait(speed);
+            $text.innerHTML += letter.shift();
+        }
+
+        // 잠시 대기
+        await wait(800);
+    };
+
+    // 딜레이 기능 ( 마이크로초 )
+    function wait(ms) {
+        return new Promise(res => setTimeout(res, ms));
     }
 
-    // 잠시 대기
-    await wait(800);
+    // 초기 실행
+    setTimeout(typing, 1500);
 
-}
-
-// 딜레이 기능 ( 마이크로초 )
-function wait(ms) {
-    return new Promise(res => setTimeout(res, ms))
-}
-
-// 초기 실행
-setTimeout(typing, 1500);
-
-
-//서브메뉴
-$(document).ready(function() {
-    $('.nav-link').on('click', function(e) {
-        e.preventDefault();
-        var targetId = '#detailNav' + this.id.slice(-1);
-        $('.detail-navbar').not(targetId).slideUp();
-        $(targetId).slideToggle();
+    // 서브메뉴
+    $(document).ready(function() {
+        $('.nav-link').on('click', function(e) {
+            e.preventDefault();
+            var targetId = '#detailNav' + this.id.slice(-1);
+            $('.detail-navbar').not(targetId).slideUp();
+            $(targetId).slideToggle();
+        });
     });
 });
